c82a2e8802c3b56e949cb94b16c20029d418bd7c
Bikramjit Singh <singhbikramjit222@gmail.com>
2022-12-30 13:32:41 -0800

    Initial Setup

--- config/application.rb
+++ config/application.rb
@@ -35,5 +35,10 @@ module BlueVelvetCoffee
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore, 
+      key: '_blue_velvet_coffee_session',
+      same_site: :lax, 
+      secure: Rails.env.production?
   end
 end

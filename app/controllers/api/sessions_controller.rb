b5a6d76c089954850f8bb3574bd7bfe8d83fb387
Bikramjit Singh <singhbikramjit222@gmail.com>
2022-12-30 17:48:44 -0800

    Session controller completed

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,30 @@
+class Api::SessionsController < ApplicationController
+  before_action :require_logged_out, only: [:create]
+  before_action :require_logged_in, only: [:destroy]
+
+  def show
+    if current_user
+      @user = current_user
+      render json: @user
+      # render "api/users/show"
+    else
+      render json: { user: nil }
+    end
+  end
+
+  def create
+    @user = User.find_by_credentials(params[:email], params[:password])
+    if @user
+      login!(@user)
+      render json: @user
+      # render "api/users/show"
+    else
+      render json: { errors: ["Incorrect email or password"] }, status: :unauthorized
+    end
+  end
+
+  def destroy
+    logout! if current_user
+    render json: { message: "success" }
+  end
+end

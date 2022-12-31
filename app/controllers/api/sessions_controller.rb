94286269e946384204a743fc43e1923ab26172c6
Bikramjit Singh <singhbikramjit222@gmail.com>
2022-12-31 13:53:05 -0800

    changed show page json to view fetch

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -5,8 +5,8 @@ class Api::SessionsController < ApplicationController
   def show
     if current_user
       @user = current_user
-      render json: @user
-      # render "api/users/show"
+      # render json: @user
+      render "api/users/show"
     else
       render json: { user: nil }
     end
@@ -16,8 +16,8 @@ class Api::SessionsController < ApplicationController
     @user = User.find_by_credentials(params[:email], params[:password])
     if @user
       login!(@user)
-      render json: @user
-      # render "api/users/show"
+      # render json: @user
+      render "api/users/show"
     else
       render json: { errors: ["Incorrect email or password"] }, status: :unauthorized
     end

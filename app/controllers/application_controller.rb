f27407bc827c4b0d543548a8e050a7eda0afd060
Bikramjit Singh <singhbikramjit222@gmail.com>
2022-12-31 15:13:38 -0800

    CSRF Protection code added along with error handling fomatting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::API
-  before_action :snake_case_params
+  include ActionController::RequestForgeryProtection
 
+  protect_from_forgery with: :exception
+  
+  before_action :snake_case_params, :attach_authenticity_token 
+
+  rescue_from StandardError, with: :unhandled_error
+  rescue_from ActionController::InvalidAuthenticityToken, with: :invalid_authenticity_token
 
   def test
     if params.has_key?(:login)
@@ -8,52 +14,70 @@ class ApplicationController < ActionController::API
     elsif params.has_key?(:logout)
       logout!
     end
-  
+
     if current_user
-      render json: { user: current_user.slice('id', 'username', 'session_token') }
+      render json: { user: current_user.slice("id", "username", "session_token") }
     else
-      render json: ['No current user']
+      render json: ["No current user"]
     end
   end
-
-
 
   #CRRLLL
   def current_user
     @current_user ||= User.find_by_session_token(session[:session_token])
-end
+  end
 
-def require_logged_in
+  def require_logged_in
     if !logged_in?
-        render json: { message: 'Unauthorized' }, status: :unauthorized
+      render json: { message: "Unauthorized" }, status: :unauthorized
     end
-end
+  end
 
-def require_logged_out
+  def require_logged_out
     if logged_in?
-        render json: { message: 'Unauthorized' }, status: :unauthorized
+      render json: { message: "Unauthorized" }, status: :unauthorized
     end
-end
+  end
 
-def login!(user)
+  def login!(user)
     session[:session_token] = user.reset_session_token!
     @current_user = user
-end
+  end
 
-def logged_in?
+  def logged_in?
     !!current_user
-end
+  end
 
-def logout!
+  def logout!
     current_user.reset_session_token!
     session[:session_token] = nil
     @current_user = nil
-end
-
+  end
 
   private
 
   def snake_case_params
     params.deep_transform_keys!(&:underscore)
+  end
+
+  def attach_authenticity_token
+    headers["X-CSRF-Token"] = masked_authenticity_token(session)
+  end
+
+  def invalid_authenticity_token
+    render json: { message: "Invalid authenticity token" },
+      status: :unprocessable_entity
+  end
+
+  def unhandled_error(error)
+    if request.accepts.first.html?
+      raise error
+    else
+      @message = "#{error.class} - #{error.message}"
+      @stack = Rails::BacktraceCleaner.new.clean(error.backtrace)
+      render "api/errors/internal_server_error", status: :internal_server_error
+
+      logger.error "\n#{@message}:\n\t#{@stack.join("\n\t")}\n"
+    end
   end
 end
